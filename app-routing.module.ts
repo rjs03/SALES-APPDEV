5f39929fd8156624ec3f7c7abf45df6f6129241e
rjs03 <ryanjellosales@gmail.com>
2024-04-05 15:16:48 +0800

    first commit

---
+++ app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'assignment',
+    pathMatch: 'full'
+  },
+  {
+    path: 'assignment',
+    loadChildren: () => import('./assignment/assignment.module').then( m => m.AssignmentPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
